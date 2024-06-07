d10b3438c4af39889f8f4ff94c0bc7de1021d2fe
Harshit Verma <147051169+harshiiit19@users.noreply.github.com>
2024-06-07 09:01:58 +0530

    Tic_Tac_Toe.py

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -1,64 +1,67 @@
-"""Tic Tac Toe
+# Tic-Tac-Toe game in Python
 
-Exercises
+board = [" " for x in range(9)]
 
-1. Give the X and O a different color and width.
-2. What happens when someone taps a taken spot?
-3. How would you detect when someone has won?
-4. How could you create a computer player?
-"""
+def print_board():
+    row1 = "| {} | {} | {} |".format(board[0], board[1], board[2])
+    row2 = "| {} | {} | {} |".format(board[3], board[4], board[5])
+    row3 = "| {} | {} | {} |".format(board[6], board[7], board[8])
 
-from turtle import *
+    print()
+    print(row1)
+    print(row2)
+    print(row3)
+    print()
 
-from freegames import line
+def player_move(icon):
+    if icon == "X":
+        number = 1
+    elif icon == "O":
+        number = 2
 
+    print("Your turn player {}".format(number))
 
-def grid():
-    """Draw tic-tac-toe grid."""
-    line(-67, 200, -67, -200)
-    line(67, 200, 67, -200)
-    line(-200, -67, 200, -67)
-    line(-200, 67, 200, 67)
+    choice = int(input("Enter your move (1-9): ").strip())
+    if board[choice - 1] == " ":
+        board[choice - 1] = icon
+    else:
+        print()
+        print("That space is taken!")
 
+def is_victory(icon):
+    if (board[0] == icon and board[1] == icon and board[2] == icon) or \
+       (board[3] == icon and board[4] == icon and board[5] == icon) or \
+       (board[6] == icon and board[7] == icon and board[8] == icon) or \
+       (board[0] == icon and board[3] == icon and board[6] == icon) or \
+       (board[1] == icon and board[4] == icon and board[7] == icon) or \
+       (board[2] == icon and board[5] == icon and board[8] == icon) or \
+       (board[0] == icon and board[4] == icon and board[8] == icon) or \
+       (board[2] == icon and board[4] == icon and board[6] == icon):
+        return True
+    else:
+        return False
 
-def drawx(x, y):
-    """Draw X player."""
-    line(x, y, x + 133, y + 133)
-    line(x, y + 133, x + 133, y)
+def is_draw():
+    if " " not in board:
+        return True
+    else:
+        return False
 
-
-def drawo(x, y):
-    """Draw O player."""
-    up()
-    goto(x + 67, y + 5)
-    down()
-    circle(62)
-
-
-def floor(value):
-    """Round value down to grid with square size 133."""
-    return ((value + 200) // 133) * 133 - 200
-
-
-state = {'player': 0}
-players = [drawx, drawo]
-
-
-def tap(x, y):
-    """Draw X or O in tapped square."""
-    x = floor(x)
-    y = floor(y)
-    player = state['player']
-    draw = players[player]
-    draw(x, y)
-    update()
-    state['player'] = not player
-
-
-setup(420, 420, 370, 0)
-hideturtle()
-tracer(False)
-grid()
-update()
-onscreenclick(tap)
-done()
+while True:
+    print_board()
+    player_move("X")
+    print_board()
+    if is_victory("X"):
+        print("X wins! Congratulations!")
+        break
+    elif is_draw():
+        print("It's a draw!")
+        break
+    player_move("O")
+    if is_victory("O"):
+        print_board()
+        print("O wins! Congratulations!")
+        break
+    elif is_draw():
+        print("It's a draw!")
+        break
